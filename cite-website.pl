e45ddd97057e5df77b2978f3f49bc2792a3ee651
Vaclav Haisman <vhaisman@gmail.com>
2016-01-22 16:04:51 +0100

    Improve.
    Get more information from other HTTP headers.
    First try on Microdata.

--- cite-website.pl
+++ cite-website.pl
@@ -7,8 +7,12 @@ use Switch;
 use Data::Dumper;
 use LWP::Simple qw();
 use Data::OpenGraph;
+use HTML::HTML5::Microdata::Parser;
+use RDF::Query;
 use YAML qw();
 use DateTime::Format::ISO8601;
+use DateTime::Format::W3CDTF;
+use DateTime::Format::HTTP;
 use IO::Handle;
 use Scalar::Util qw(reftype);
 
@@ -32,8 +36,10 @@ sub test {
     my $x = $_[0];
     return defined ($x)
         && ((ref $x ne ''
-             && reftype($x) eq 'ARRAY'
-             && scalar @$x)
+             && ((reftype($x) eq 'ARRAY'
+                  && scalar @$x)
+                 || (reftype($x) eq 'HASH'
+                     && scalar keys %$x)))
             || length ($x));
 }
 
@@ -42,7 +48,7 @@ sub read_og_array {
     my ($og, $prop) = @_;
     my $prop_value = $og->property($prop);
     my @arr;
-    if (test $prop_value) {
+    if (test($prop_value)) {
         if (ref $prop_value ne ''
             && reftype($prop_value) eq 'ARRAY') {
             @arr = @{$prop_value};
@@ -75,13 +81,37 @@ sub date_conversion {
 }
 
 
+sub parse_author {
+    my $text = $_[0];
+    my $author = {};
+
+    if ($text =~ /^\s*([^,]+)\s*,\s*(.*\S)\s*$/) {
+        my $family = $1;
+        my $given = $2;
+        $author->{'family'} = $family;
+        $author->{'given'} = $given;
+    }
+    elsif ($text =~ /^\s*(\S+\s+|\S+)+\s+(\S+)\s*$/) {
+        my $family = $2;
+        my $given = $1;
+        $author->{'family'} = $family;
+        $author->{'given'} = $given;
+    }
+    else {
+        $author->{'family'} = $text;
+    }
+
+    return $author;
+}
+
+
 my $title = $og->property('title');
-if (test $title) {
+if (test($title)) {
     $entry{'title'} = $title;
 }
 
 my $url = $og->property('url');
-if (test $url) {
+if (test($url)) {
     $entry{'URL'} = $url;
 }
 else {
@@ -89,12 +119,12 @@ else {
 }
 
 my $publisher = $og->property('site_name');
-if (test $publisher) {
+if (test($publisher)) {
     $entry{'publisher'} = $publisher;
 }
 
 my $og_type = $og->property('_basictype');
-if (test $og_type) {
+if (test($og_type)) {
     my $type;
     switch ($og_type) {
         case "article" { $type = "article"; }
@@ -106,6 +136,7 @@ if (test $og_type) {
 }
 else {
     Carp::carp("og:type attribute is missing.");
+    $entry{'type'} = "website";
 }
 
 my $id = undef;
@@ -117,23 +148,7 @@ if ($og_type) {
         foreach my $creator (@creators) {
             #print STDERR "creator: >$creator<\n";
             my $creator_text = $creator;
-            my $author = {};
-            if ($creator_text =~ /^\s*([^,]+)\s*,\s*(.*\S)\s*$/) {
-                my $family = $1;
-                my $given = $2;
-                $author->{'family'} = $family;
-                $author->{'given'} = $given;
-            }
-            elsif ($creator_text =~ /^\s*(\S+\s+|\S+)+\s+(\S+)\s*$/) {
-                my $family = $2;
-                my $given = $1;
-                $author->{'family'} = $family;
-                $author->{'given'} = $given;
-            }
-            else {
-                $author->{'family'} = $creator_text;
-            }
-
+            my $author = parse_author($creator_text);
             if (! $id) {
                 $id = $author->{'family'};
                 $id =~ s/\W//g;
@@ -155,7 +170,7 @@ if ($og_type) {
     }
 
     #print STDERR "published time: ", $og_issued_date_str // "(undef)", "\n";
-    if (test $og_issued_date_str) {
+    if (test($og_issued_date_str)) {
         my $date = DateTime::Format::ISO8601->parse_datetime($og_issued_date_str);
         my $year = $date->year;
 
@@ -167,8 +182,14 @@ if ($og_type) {
     }
 
     if ($og_type eq 'book'
-        && test $og->property("$og_type:isbn")) {
+        && test($og->property("$og_type:isbn"))) {
         $entry{'ISBN'} = $og->property("$og_type:isbn");
+    }
+
+    if (! test($publisher)
+        && test($og->property("$og_type:publisher"))) {
+        $publisher = $og->property("$og_type:publisher");
+        $entry{'publisher'} = $publisher;
     }
 
     my @og_tags = read_og_array ($og, "$og_type:tag");
@@ -179,6 +200,44 @@ if ($og_type) {
     }
 
 } # $og_type
+
+my $microdata = HTML::HTML5::Microdata::Parser->new (
+    $htmldoc, $ARGV[0]);
+    #{auto_config => 1, tdb_service => 1, xhtml_meta => 1, xhtml_rel => 1});
+#print STDERR "microdata->graph:\n", Dumper($microdata->graph), "\n";
+
+my $query = RDF::Query->new(<<'SPARQL');
+PREFIX schema: <http://schema.org/>
+SELECT *
+WHERE {
+   ?author a schema:Person .
+}
+SPARQL
+
+my $people = $query->execute($microdata->graph);
+#print STDERR "authors from RDF:\n", Dumper($people), "\n";
+while (my $person = $people->next) {
+    print STDERR "people: ", $person, "\n";
+}
+
+my $html_headers = HTTP::Headers->new;
+my $headers_parser = HTML::HeadParser->new($html_headers);
+$headers_parser->parse($htmldoc);
+
+if (! exists $entry{'author'}
+    && test($html_headers->header('X-Meta-Author'))) {
+    my $author = parse_author($html_headers->header('X-Meta-Author'));
+    push @{$entry{'author'}}, $author;
+}
+
+my $html_meta_date_str = $html_headers->header('X-Meta-Date');
+if (! exists $entry{'issued'}
+    && test($html_meta_date_str)) {
+    #print STDERR "date: >", $html_meta_date_str, "<\n";
+
+    my $date = DateTime::Format::HTTP->parse_datetime($html_meta_date_str);
+    $entry{'issued'} = date_conversion $date;
+}
 
 $entry{'accessed'} = date_conversion(DateTime->today());
 
